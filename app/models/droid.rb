771fd45f697eecbb270831310c8e816f43d2a9fc
Gautier Lapeyronie <goochlapeyronie@gmail.com>
2021-02-03 10:00:04 +0100

    models done

--- app/models/droid.rb
+++ app/models/droid.rb
@@ -1,3 +1,7 @@
 class Droid < ApplicationRecord
   belongs_to :user
+  validates :name, :address, :description, :price, presence: true
+
+  has_many :categories, through: :droid_categories
+  has_many :bookings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, uniqueness: true, presence: true
+  validates :email,
+            format: { with: /^(.+)@(.+)$/, message: "Email invalid" },
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 4, maximum: 254 }
+  validates :password,
+            presence: true,
+            length: { minimum: 6 }
+
+  has_many :droids, dependent: :destroy
 end
