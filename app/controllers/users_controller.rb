def04de5abacdef2b1b759e0d5a41bd83dfc225e
Gautier Lapeyronie <goochlapeyronie@gmail.com>
2021-02-09 20:50:42 +0100

    user show back done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    # @bookings = Booking.where(user_id: current_user)
     @droids = current_user.droids
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,8 @@ class Booking < ApplicationRecord
   belongs_to :droid
 
   validates :start_date, :end_date, presence: true
+
+  def find_user(user_id)
+    User.find(user_id).name
+  end
 end
