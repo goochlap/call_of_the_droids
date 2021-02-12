c54446452527af5a20ccec06ee8108af74cdfa8c
anthony dupuis <anthonydupuis95@yahoo.fr>
2021-02-12 23:29:34 +0100

    almost done with views

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,14 @@
+// const initUpdateNavbarOnScroll = () => {
+//   const navbar = document.querySelector('.navbar-default');
+//   if (navbar) {
+//     window.addEventListener('scroll', () => {
+//       if (window.scrollY >= window.innerHeight) {
+//         navbar.classList.add('navbar-default-black');
+//       } else {
+//         navbar.classList.remove('navbar-default-black');
+//       }
+//     });
+//   }
+// }
+
+// export { initUpdateNavbarOnScroll };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,9 @@ window.addEventListener("DOMContentLoaded", event => {
   audio.volume = 1;
   audio.play();
 });
+
+
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -38,4 +41,12 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+// import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+// document.addEventListener('turbolinks:load', () => {
+//   // Call your JS functions here
+//   initUpdateNavbarOnScroll();
+// });
+
 
