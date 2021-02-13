01444e7c61c5e8ee91ce9e50192451360d388a19
anthony dupuis <anthonydupuis95@yahoo.fr>
2021-02-13 14:03:51 +0100

    before heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    # @bookings = Booking.where(user_id: current_user)
+    @bookings = Booking.where(user_id: current_user)
     @droids = current_user.droids
   end
 end
--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,14 +1,14 @@
-// const initUpdateNavbarOnScroll = () => {
-//   const navbar = document.querySelector('.navbar-default');
-//   if (navbar) {
-//     window.addEventListener('scroll', () => {
-//       if (window.scrollY >= window.innerHeight) {
-//         navbar.classList.add('navbar-default-black');
-//       } else {
-//         navbar.classList.remove('navbar-default-black');
-//       }
-//     });
-//   }
-// }
+const initUpdateNavbarOnScroll = () => {
+  const navbar = document.querySelector('.navbar-default');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 100) {
+        navbar.classList.add('navbar-default-black');
+      } else {
+        navbar.classList.remove('navbar-default-black');
+      }
+    });
+  }
+}
 
-// export { initUpdateNavbarOnScroll };
+export { initUpdateNavbarOnScroll };
